1328ff9215e81ac483f09c80f907646c0ac6d1c1
Amadavid <anihgrace200@gmail.com>
2024-10-23 01:37:34 +0100

    linking the pages to the home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import About from './pages/About'
+
+
 
 const App = () => {
   return (
-    <div > 
+    <div className='mx -4 sm:mx-[10%]'> 
+
+    <Routes>
+
+      <Route path='/' element={<Home />} />
+      <Route path='/doctors' element={<Doctors />} />
+      <Route path='/doctors/:speciality' element={<Doctors />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/my-appiontments' element={<MyAppiontments />} />
+      <Route path='/My-Profile' element={<MyProfile />} />
+      <Route path='/appointment' element={<Appointments />} />
+
+
+      
+    </Routes>
       
     </div>
   )
