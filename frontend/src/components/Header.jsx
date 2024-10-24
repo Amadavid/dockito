7744180f9fb93b67638b398989ce33e85a0c0461
Amadavid <anihgrace200@gmail.com>
2024-10-24 12:48:10 +0100

    create header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,6 +3,24 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
+        {/* -------leftside -------*/}
+        <div>
+            <p>
+                Book Appointment <br /> With Amazing Doctors
+            </p>
+            <div>
+                <img src={assets.group_profiles} alt="" />
+                <p>Go through our list of amazing doctors,<br /> schedule your appointment </p>
+            </div>
+            <a href="">
+                Book appointment <img src={assets.arrow_icon} alt="" />
+            </a>
+        </div>
+
+        {/* -------Right Side ------ */}
+        <div>
+
+        </div>
       
     </div>
   )
