42a315fc5b09c383127e309109ac6d9260eed73b
Amadavid <anihgrace200@gmail.com>
2024-10-24 14:22:04 +0100

    add css to header

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,13 +6,13 @@ const Navbar = () => {
 
     const Navigate = useNavigate();
 
-    const [showMenu, setShowMenu] = useState(false)
+    const [showMenu,setShowMenu] = useState(false)
     const [token,setToken] = useState(true)
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-purple-400'>
         <img className='w-44 cursor-pionter' src={assets.logo} alt="" />
         <ul className='hidden md:flex items-start gap-5 font-medium'>
-            <NavLink to='/home'>
+            <NavLink to='/'>
                 <li className='py-1'>HOME</li>
                 <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
             </NavLink>
