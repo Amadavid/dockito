fa97d1ddbeac0aa8de542338afaee21ffb0f9b37
Amadavid <anihgrace200@gmail.com>
2024-11-05 07:28:53 +0100

    Add appointment retrieval API for doctors

--- Backend/controllers/doctorController.js
+++ Backend/controllers/doctorController.js
@@ -1,7 +1,8 @@
 import jwt from 'jsonwebtoken'; // Import jwt
 import bcrypt from 'bcrypt'; // Import bcrypt
-import User from '../models/userModel.js'; // Import the user model
-import Doctor from '../models/doctorModel.js'; // Import the doctor model
+import doctorModel from '../models/doctorModel.js';
+import appointmentModel from '../models/appointmentModel.js';   // Import the appointment model
+
 
 // Api for doctor to login
 const loginDoctor = async (req, res) => {
@@ -25,5 +26,21 @@ const loginDoctor = async (req, res) => {
     }catch (err) {
         console.log(err) // Log the error
         res.json({ success: false, message: err.message }) // Return the error message
+    }
+}
+
+// Api to get the doctor appointments from the database
+
+const appointmentsDoctor = async (req, res) => {      // Get the appointments of the doctor
+    try {
+
+        const { docId } = req.body
+        const appointments = await appointmentModel.find({ docId })
+
+        res.json({ success: true, appointments })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
     }
 }
