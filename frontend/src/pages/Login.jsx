778514c7c96ab4517ecbafab28e156b7d5cacab3
Amadavid <anihgrace200@gmail.com>
2024-11-13 10:59:53 +0100

    Refactor Login component to include useContext and useEffect hooks for improved state management

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../context/AppContext'
 import axios from 'axios'
 import { toast } from 'react-toastify'
