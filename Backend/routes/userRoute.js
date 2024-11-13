1dce62a241fc0d48988a45fa11c8ab6bb26cc996
Amadavid <anihgrace200@gmail.com>
2024-11-13 10:55:45 +0100

    Add getUserAppointments route and refactor userRouter for consistency

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,19 +1,20 @@
 import express from 'express';
-import { loginUser, registerUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment, paymentStripe, verifyStripe } from '../controllers/userController.js';
+import { loginUser, registerUser, getProfile, updateProfile, bookAppointment, listAppointment, getUserAppointments,cancelAppointment, paymentStripe, verifyStripe } from '../controllers/userController.js';
 import authUser from '../middleware/authUser.js';
 import upload from '../middleware/multer.js';
-
 const userRouter = express.Router();
 
-userRouter.post("/register", registerUser);
-userRouter.post("/login", loginUser);
 
-userRouter.get("/get-profile", authUser, getProfile);
-userRouter.post("/update-profile", authUser, upload.single('image'), updateProfile);
-userRouter.post("/book-appointment", authUser, bookAppointment);
+userRouter.post("/register", registerUser)
+userRouter.post("/login", loginUser)
+
+userRouter.get("/get-profile", authUser, getProfile)
+userRouter.post("/update-profile", authUser, upload.single('image'), updateProfile)
+userRouter.post("/book-appointment", authUser, bookAppointment)
 userRouter.get("/list-appointments", authUser, listAppointment);
-user.post("/cancel-appointment", authUser, cancelAppointment);
-user.post("/payment-stripe", authUser, paymentStripe);
-user.post("/verify-stripe", authUser, verifyStripe);
+userRouter.get('/appointments', authUser, getUserAppointments)
+userRouter.post("/cancel-appointment", authUser, cancelAppointment)
+userRouter.post("/payment-stripe", authUser, paymentStripe)
+userRouter.post("/verify-stripe", authUser, verifyStripe)
 
 export default userRouter;
