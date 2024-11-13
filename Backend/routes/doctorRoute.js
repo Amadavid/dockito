a39a0f1f3c0da94ad8255789807f3cee195ead87
Amadavid <anihgrace200@gmail.com>
2024-11-13 10:55:24 +0100

    Change appointments route method from POST to GET for improved API design

--- Backend/routes/doctorRoute.js
+++ Backend/routes/doctorRoute.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { loginDoctor, appointmentsDoctor, appointmentCancel, doctorList, changeAvailability,appointmentComplete, doctorDashboard, doctorProfile, updateDoctorProfile } from '../controllers/doctorController.js';
+import { loginDoctor, appointmentsDoctor, appointmentCancel, doctorList, changeAvailability, appointmentComplete, doctorDashboard, doctorProfile, updateDoctorProfile } from '../controllers/doctorController.js';
 import authDoctor from '../middleware/authDoctor.js';
 
 const doctorRouter = express.Router();
 
 doctorRouter.post('/login', loginDoctor);
-doctorRouter.post("/appointments", authDoctor, appointmentsDoctor);
+doctorRouter.get("/appointments", authDoctor, appointmentsDoctor);
 doctorRouter.post("/cancel-appointment", authDoctor, appointmentCancel);
 doctorRouter.get("/doctor-list", doctorList);
 doctorRouter.post("/change-availability", authDoctor, changeAvailability);
